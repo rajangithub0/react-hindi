3bd1a4767b5b50599fbd04329a68d62f6cfeb701
rajan <rajan.rana6@gamil.com>
2025-10-29 00:04:12 +0530

    project 3 currency application

---
+++ 06_currency_convertor/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./index.css";
+
+
+function App() {
+  const []=useState(0)
+  return (
+    <>
+      <h1 className=" text-center text-3xl bg-orange-500" >Currency App</h1>
+    </>
+  );
+}
+
+export default App;
